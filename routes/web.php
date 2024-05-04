c799771fb3044011bd45725179da5e2f3bcbbba9
Blade Lucas <elffe2@hotmail.ca>
2024-05-04 01:02:37 -0400

    Added image assets and main pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+
+Route::get('/server', function () {
+    return view('server');
+});
+
+Route::get('/downloads', function () {
+    return view('downloads');
 });
